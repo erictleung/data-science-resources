30545ed406ee5bb5acbbd25e2b966e041e5deed3
Eric Leung <leunge@ohsu.edu>
2016-05-18 11:59:12 -0700

    adjacency.py: show matrix encoding of networks

---
+++ bin/networks/adjacency.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Description:  Demonstrate adjacency matrix notation for networks
+# Author:       Eric Leung
+# Date:         2016 May 18th
+# Version:      3.5.1
+
+import numpy  # 1.11.0
+
+# Initialize simple network in adjacency matrix
+A = [[0, 1, 0, 0, 1, 0],
+     [1, 0, 1, 1, 0, 0],
+     [0, 1, 0, 1, 1, 1],
+     [0, 1, 1, 0, 0, 0],
+     [1, 0, 1, 0, 0, 0],
+     [0, 0, 1, 0, 0, 0]]
+A = np.array(A)
+
+
+# Create a weighted network with edges having different value
+B = [[0, 2, 1],
+     [2, 0, 0.5],
+     [1, 0.5, 0]]
+B = np.array(B)
+
+
+# Create directed network
+C = [[0, 0, 0],
+     [1, 0, 0],
+     [0, 1, 0]]
+C = np.array(C)
